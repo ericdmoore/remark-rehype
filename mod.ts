014c17bbd45fd5ff49482b9052804033ca359d1f
Eric Moore <eric.d.moore@gmail.com>
2022-03-01 22:28:36 -0600

    ready and steady the hounds

---
+++ mod.ts
@@ -0,0 +1,12 @@
+/**
+ * @typedef {import('./lib/index.js').Options} Options
+ * @typedef {import('./lib/index.js').Processor} Processor
+ */
+
+export {defaultHandlers, all, one} from './node_modules/mdast-util-to-hast/index.js'
+
+import rmrh from './lib/index.js'
+export const remarkRehype = rmrh
+export default rmrh
+
+console.log(rmrh)
